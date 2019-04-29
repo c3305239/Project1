7f57429c6258e2e4603d7ec1fcca7f60bb039e7e
c3305239 <48540640+c3305239@users.noreply.github.com>
2019-04-29 16:50:00 +1000

    Update main.c

--- main.c
+++ main.c
@@ -1,37 +1,100 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(){
+void rotdecipher(char *input, char *output, int rot) {
     
-    /*Will be used for storing the user's choice on 
-    rotation/substitution cipher and either encryption or decryption of text*/
-    int programchoice;
+    for (; *input != '\0'; input++, output++) {
+        int c = *input - 65; // Character A = 0, so deduct 65 from the input character.
+        if (c >= 0 && c <= 26) {
+            c = (c + rot) % 26;
+            if (c > 25) c = c - 26;
+            *output = c + 65;
+        } else {
+            *output = *input;
+        }
+    }
+    *output = '\0';
+}
+
+void rotcipher(char *input, char *output, int rot) {
+    for (; *input != '\0'; input++, output++) {
+        int c = *input - 65;
+         if (c >= 0 && c <= 26) {
+                c = (c - rot) % 26;
+                if (c < 0) c = c + 26;
+                *output = c + 65;
+            } else {
+                *output = *input;
+        }
+    }
+    *output = '\0';
+}
+
+int strpos(char *haystack, char needle)
+{
+    char *p = strchr(haystack, needle);
+    if (p)
+        return (int)(p - haystack);
+    return -1;
+}
+
+void subcipher(char *cipher, char *input, char *output) {
+    for (; *input != '\0'; input++, output++) {
+        char c = *input;
+        if (c >= 65 && c <= 90) {
+            int i =  cipher[c-65];
+            *output = i;
+        } else {
+            *output = *input;
+        }
+    }
+    *output = '\0';
+}
+
+void unsubcipher(char *cipher, char *input, char *output) {
+    for (; *input != '\0'; input++, output++) {
+        char c = *input;
+        if (c >= 65 && c <= 90) {
+            int i =  strpos(cipher, c);
+            *output = i + 65;
+        } else {
+            *output = *input;
+        }
+    }
+    *ouput = '\0';
+}
+
+//void unsubcipher(
+
+int main(int argc, const char * argv[]) {
     
-    //Declares the max length of text that the program will process
-    char str[1000];
     
-    //Menu for user to choose from
-    printf("To encrypt with a rotation cipher press 1 then ENTER\n");
-    printf("To decrypt with a rotation cipher press 2 then ENTER\n");
-    printf("To encrypt with a substitution cipher press 3 then ENTER\n");
-    printf("To decrypt with a substitution cipher press 4 then ENTER\n");
+    char input[] = "ATTACK AT SUNRISE";
+    char cipherText[] = "ZSSZBJ ZS RTMQHRD";
+    char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char cipher[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    char subCipherText[] = "HSTQLT UTZ DOSA QZ ZIT LIGHL";
     
-    //Stores user's choice at "programchoice"
-    scanf("%d", &programchoice);
+    char output[1000]; // Our Maximum output size
     
-    printf("Please enter text\t");
-    scanf("%[^\n]%*c", str);
-   // printf("%d", str);
-   
-   printf("The text is %s \n", str);
-    
-   /* switch(x){
-        case 1:
-            for(programchoice = 1);
-        
-        
-        
-    }*/
+    int key = 1;
     
     
-   return 0; 
+    //unsubcipher(cipher, subCipherText, output);
+    //printf("output:%s", output);
+     
+    
+    //rotcipher(alphabet, output, key);
+    //printf("cipherText encoded: %s\n",output);
+    
+    /* Encode then Decode
+    rotcipher(input, output, key);
+    printf("Encoded %s\n",output);*/
+    
+    /* Decode the encoded
+    rotdecipher(output, output, key);
+    printf("Decoded %s\n",output);
+    */
+    return 0;
+    
 }
